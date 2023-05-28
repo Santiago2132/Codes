e35f29d872eee596158f7e98d6c72c7f263c9937
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2023-05-28 16:56:31 -0500

    Se termino el ejercico unidad 4

--- Introduccion a programacion/Ejercicios tema 4/src/Main.java
+++ Introduccion a programacion/Ejercicios tema 4/src/Main.java
@@ -1,17 +1,63 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+        //Pista: Los números inferiores a 0 son negativos y los superiores, positivos.
+        System.out.println("Ejecución del If");
+        int numeroIf = 0;
+        if(numeroIf > 0){
+            System.out.println("Es mayor que 0");
+        }else if(numeroIf < 0){
+            System.out.println("Es menor que 0");
+        }else {
+            System.out.println("Es 0");
+        }
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        //Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3, el bloque de código que tendrá el bucle deberá:
+            //Incrementar el valor de la variable en uno cada vez que se ejecute.
+            //Mostrarlo por pantalla cada vez que se ejecute.
+        System.out.println("Ejecución while");
+        int numeroWhile = 0;
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        while(numeroWhile<3){
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        }
+        //Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
+        int numeroDoWhile = 0;
+        do{
+            System.out.println("Ejecución del do while \n"+ numeroDoWhile);
+            numeroDoWhile++;
+        }while (numeroWhile==0);
+
+        //Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y su condición será que
+        //la variable sea igual o menor que 3, se irá incrementando en 1 su valor cada vez que se ejecute y deberá mostrarse por pantalla.
+        System.out.println("Ejecución del for");
+        int numeroFor = 0;
+
+        for(int i =0; i <= 3; i++){
+            System.out.println(numeroFor++);
+        }
+        //Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro estaciones del año. Dependiendo del valor de la variable estacion se deberá mandar un mensaje
+        // por consola informando de la estación en la que está. También habrá que poner un default para cuando el valor de la variable no sea una estación.
+
+        String estacion = "verano";
+
+        switch (estacion) {
+            case "invierno":
+                System.out.println("Es invierno, hace frío");
+                break;
+            case "primavera":
+                System.out.println("Es primavera, las flores están floreciendo");
+                break;
+            case "verano":
+                System.out.println("Es verano, hace calor");
+                break;
+            case "otoño":
+                System.out.println("Es otoño, las hojas cambian de color");
+                break;
+            default:
+                System.out.println("La estación no es válida");
         }
     }
 }
