1403f2b5df701b8e2c26b97913e17118d0e2bcf6
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2023-05-29 22:10:22 -0500

    Se termino de comentar el ejercicio

--- Introduccion a programacion/Ejercicios tema 9/src/Main.java
+++ Introduccion a programacion/Ejercicios tema 9/src/Main.java
@@ -1,3 +1,10 @@
+//Crea una clase Persona con las siguientes variables:
+// + La edad
+// + El nombre
+// + El teléfono
+//Una vez creada la clase, crea una nueva clase Cliente que herede de Persona, esta nueva clase tendrá la variable credito solo para esa clase.
+//Crea ahora un objeto de la clase Cliente que debe tener como propiedades la edad, el telefono, el nombre y el credito, tienes que darles valor y mostrarlas por pantalla.
+//Una vez hecho esto, haz lo mismo con la clase Trabajador que herede de Persona, y con una variable salario que solo tenga la clase Trabajador.
 class Persona {
     int edad;
     String nombre;
@@ -65,7 +72,6 @@ class Trabajador extends Persona{
         this.salario = salario;
     }
 }
-
 public class Main {
     public static void main(String[] args) {
         Persona persona = new Persona(12,"Andres","124124");
