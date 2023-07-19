854f22cf36ae5abc0caf4a2db8d7b9a4f3c64b98
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2023-07-19 13:46:20 -0500

    Se agrego una parte inicial del

---
+++ CodeWars/DecodeTheMorseCode/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Exercise Decode The Morse Code Part of Series 1/3
+ * The Morse Code encodes every character as a sequence of "dots"
+ * and "dashes". For example A is coded as ._
+ * Author: Santiago Maldonado Rojas
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ CodeWars/DecodeTheMorseCode/src/Morse.java
@@ -0,0 +1,18 @@
+public class Morse {
+    // Variables Decode
+    public String[] letters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    public String[] numbers = {"1","2","3","4","5","6","7","8","9","0"};
+    // Variable Code
+    public String[] lettersCode = {".-",};
+    public String[] numbersCode = {".----","..---","...--","....-",".....","-....","--...","---..","----.","-----"};
+
+    public String Code(String text){
+        return null;
+    }
+    public String Decode(String text){
+        return null;
+    }
+    public static void main(String [] args){
+        System.out.println();
+    }
+}
