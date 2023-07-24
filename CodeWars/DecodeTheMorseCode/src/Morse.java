7a14e7650d4bb73d8a6d2eb1ab7d387bf45d6032
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2023-07-24 07:55:14 -0500

    Se agregaron cambios

--- CodeWars/DecodeTheMorseCode/src/Morse.java
+++ CodeWars/DecodeTheMorseCode/src/Morse.java
@@ -1,10 +1,10 @@
+import java.util.ArrayList;
+
 public class Morse {
-    // Variables Decode
     public String[] letters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-    public String[] numbers = {"1","2","3","4","5","6","7","8","9","0"};
-    // Variable Code
     public String[] lettersCode = {".-",};
-    public String[] numbersCode = {".----","..---","...--","....-",".....","-....","--...","---..","----.","-----"};
+    public String[] numbers = {"1","2","3","4","5","6","7","8","9","0"};
+    public String[] numbersCode = {".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----"};
 
     public String Code(String text){
         return null;
